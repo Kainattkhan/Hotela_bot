286ead0d684460b3cdb2f6eece3fd92b3c8a52d3
Kainat <125871941+Kainattkhan@users.noreply.github.com>
2025-03-14 16:22:52 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import requests
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.document_loaders import AsyncHtmlLoader, PyPDFLoader, UnstructuredWordDocumentLoader
+from langchain_community.document_loaders import AsyncHtmlLoader, PyPDFLoader, UnstructuredWordDocumentLoader
 from langchain_core.tools.retriever import create_retriever_tool
 
 # Load environment variables
