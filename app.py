84f7c3da1ace5672b4cf9daa1d2394496fc5c4d5
Kainattkhan <kainatabrahim@gmail.com>
2025-05-01 16:12:04 +0500

    optimized

--- app.py
+++ app.py
@@ -70,10 +70,9 @@ class ChatRequest(BaseModel):
 # Helper functions
 def setup_selenium_driver():
     chrome_options = Options()
-    # chrome_options.binary_location = "/opt/google/chrome/chrome"
-    # chrome_options.binary_location = "/usr/bin/chromium-browser"
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--disable-software-rasterizer")
@@ -82,11 +81,14 @@ def setup_selenium_driver():
     chrome_options.add_argument("--disable-accelerated-2d-canvas")
     chrome_options.add_argument("--no-zygote")
     chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument("--remote-debugging-port=9222")
+    # chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36")
-    # service = Service("/usr/local/bin/chromedriver")
     service = Service("/usr/lib/chromium-browser/chromedriver")
-    return webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.set_page_load_timeout(30) 
+    # return driver
+    # webdriver.set_page_timeout(30)
+    # return webdriver.Chrome(service=service, options=chrome_options)
 
 def fetch_website_content(urls):
     if isinstance(urls, str):
